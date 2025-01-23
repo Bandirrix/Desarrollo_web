ab745506587068655c251cf95883267094cfc9b8
Bandirrix <corona.fer07@gmail.com>
2025-01-23 18:51:15 +0000

    Metodos de python

--- Python/prueba.py
+++ Python/prueba.py
@@ -1,1 +1,10 @@
 print("Hola mundo, este soy yo desde la tablet")
+frase = "Controlar la complejidad es la esencia de la programación"
+palabra = frase[:9]
+print(palabra)
+frase1 = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+letra = frase1[8::3]
+print(letra)
+frase2 = "Es genial trabajar con ordenadores. No discuten, lo recuerdan todo y no se beben tu cerveza"
+inverso = frase2[::-1]
+print(inverso)
