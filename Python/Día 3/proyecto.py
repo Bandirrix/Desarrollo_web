4e5358a4e979bd81bcd1f4f511a74d62a739f241
Bandirrix <corona.fer07@gmail.com>
2025-01-31 00:35:50 -0600

    Solo falta el uso de count para terminar el proyecto

--- Python/Día 3/proyecto.py
+++ Python/Día 3/proyecto.py
@@ -9,5 +9,13 @@ print('Tambien te pediremos que ingreses 3 letras aleatorias separadas por un es
 texto = input('Ingresa aquí tu texto a elección: ')
 letras = input('Ingresa aqui tus letras: ')
 lista_letras = letras.split()
+lista_texto = texto.split()
+texto_inverso = texto[::-1]
+texto_min = texto.lower()
+print('Tu texto tiene un total de palabras de: {}'.format(len(lista_texto)))
+print('Tu primer letra es ' + texto[0] + ' y la ultima es ' + texto[-1])
+palabra = 'Python' in texto
+print('Python se encuentra dentro del texto que escribiste??: {}'.format(palabra))
 print(texto)
 print(letras) 
+print(texto_inverso)
