b92f39b7d7d68117ed303cf4117d27ff0d678548
Bandirrix <corona.fer07@gmail.com>
2025-01-30 19:16:31 -0600

    Inicio del proyecto

---
+++ Python/Día 3/proyecto.py
@@ -0,0 +1,13 @@
+# Crear un programa que pida al usuario ingresar un texto y que ingrese tres letras a su eleccion 
+# Cuantas veces aparecen las letras que eligió 
+# Cuantas palabras hay en el texto
+# Cual es la primer letra y la ultima 
+# Mostrar el texro en modo inverso 
+# Ver si aparece la palabra {'Python'} en el texto
+print('Bienvenido al analizar de textos, a continuacion le pediremos que ingreses una seria de palabras a tu eleccion, por lo que continua adelante')
+print('Tambien te pediremos que ingreses 3 letras aleatorias separadas por un espacio')
+texto = input('Ingresa aquí tu texto a elección: ')
+letras = input('Ingresa aqui tus letras: ')
+lista_letras = letras.split()
+print(texto)
+print(letras) 
