ea1dd5529a680b837cf7b242b103f5f23edb2446
Bandirrix <corona.fer07@gmail.com>
2025-04-08 21:42:24 -0600

    b

--- Python/Día 4/proyecto_dia_4.py
+++ Python/Día 4/proyecto_dia_4.py
@@ -2,24 +2,24 @@ from random import *
 
 print('Bienvenido a adivina el numero mi estimado rey')
 nombre = input("Cuál es tu nombre mi extraño viajero?: ")
-print(f"Estás listo para jugar {nombre}? Vamos a ello que tienes que adivinar un numero del 1 al 50")
-numero_user = int(input("Dime un valor numerico entre el rango que acordamos"))
+print(f"Estás listo para jugar {nombre}? Vamos a ello que tienes que adivinar un numero del 1 al 100")
+numero_user = int(input("Dime un valor numerico entre el rango que acordamos: "))
 numero_maquina = randint(1,100)
 intentos = 8
 print(numero_maquina)
 while intentos > 0:
 	if numero_user == numero_maquina:
-    	print(f'Has ganado {nombre}, numero que pensé era: {numero_maquina}')
-    	print(f'tomado Te ha solo: {intentos} intentos ')
-	elif numero_user < 1 or numero_maquina > 100:
-    	print('Has escogido un número que está fuera del rango :()')
-	elif numero_user < numero_maquina:
-    	print('Tu numero es más pequeño del que yo elegi, sigue intentando')
+		print('Acertaste')
+	elif numero_user > 100 and numero_user < 1:
+		print('No has esogido el valor que habíamos acordado, inicia el programa de nuevo')
+		break
 	elif numero_user > numero_maquina:
-    	print('El numero que elegiste es mayor al que yo tengo, vuelve a intentarlo')
-else:
-	intentos = intentos -1
-	numero_user = int(input('Dime otro numero: '))
+		print('Tu numero es mas grande que el mio, escoge otro: ')
+	elif numero_user < numero_maquina:
+		print('Tu numero es mas pequeño que el mio, escoge otro: ')
+intentos = intentos =- 1
+numero_user = int(input(':'))
+
         
 
 
