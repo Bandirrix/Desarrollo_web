6c1aa7db296c6bc1f1b73656e8263e5970e8b0cc
Bandirrix <corona.fer07@gmail.com>
2025-04-11 03:47:59 +0000

    Terminacion del proyecto

--- Python/Día 4/proyecto_dia_4.py
+++ Python/Día 4/proyecto_dia_4.py
@@ -2,23 +2,31 @@ from random import *
 
 print('Bienvenido a adivina el numero mi estimado rey')
 nombre = input("Cuál es tu nombre mi extraño viajero?: ")
-print(f"Estás listo para jugar {nombre}? Vamos a ello que tienes que adivinar un numero del 1 al 100")
+print(f"Estás listo para jugar {nombre}? \nVamos a ello, que tienes que adivinar un numero del 1 al 100")
 numero_user = int(input("Dime un valor numerico entre el rango que acordamos: "))
 numero_maquina = randint(1,100)
-intentos = 8
-print(numero_maquina)
-while intentos > 0:
+intentos = 1
+while intentos < 8:
 	if numero_user == numero_maquina:
-		print('Acertaste')
-	elif numero_user > 100 and numero_user < 1:
+		print('Acertaste :D')
+		print(f'Lo has logrado en: {intentos} intentos')
+		break
+	elif numero_user > 100 or numero_user < 1:
 		print('No has esogido el valor que habíamos acordado, inicia el programa de nuevo')
 		break
-	elif numero_user > numero_maquina:
+	elif numero_user > numero_maquina and numero_user < 101:
+		intentos = intentos +1
 		print('Tu numero es mas grande que el mio, escoge otro: ')
-	elif numero_user < numero_maquina:
+		numero_user = int(input('Intenta con otro número: '))
+	elif numero_user < numero_maquina and numero_user < 101:
+		intentos= intentos +1
 		print('Tu numero es mas pequeño que el mio, escoge otro: ')
-intentos = intentos =- 1
-numero_user = int(input(':'))
+		numero_user = int(input('Intenta con otro número: '))
+else: 
+	print(f'Lo siento {nombre} :(, no pudiste adivinar el numero')
+	print(f'El número que he escogido era el: {numero_maquina}')
+		
+
 
         
 
